6360fbfdf49d25c7c0958d5a3490ae375b6077ae
User <kracnai.ximia@gmail.com>
2025-03-27 15:20:07 +0500

    Добавьте файлы проекта.

---
+++ LB1/FormMain.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace LB1
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            LabelInfAboutUsers = new Label();
+            panelTop = new Panel();
+            labelUsers = new Label();
+            panel1.SuspendLayout();
+            panelTop.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(LabelInfAboutUsers);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 70);
+            panel1.Margin = new Padding(5);
+            panel1.Name = "panel1";
+            panel1.Padding = new Padding(10);
+            panel1.Size = new Size(684, 491);
+            panel1.TabIndex = 0;
+            // 
+            // LabelInfAboutUsers
+            // 
+            LabelInfAboutUsers.Dock = DockStyle.Fill;
+            LabelInfAboutUsers.Location = new Point(10, 10);
+            LabelInfAboutUsers.Name = "LabelInfAboutUsers";
+            LabelInfAboutUsers.Size = new Size(664, 471);
+            LabelInfAboutUsers.TabIndex = 0;
+            // 
+            // panelTop
+            // 
+            panelTop.Controls.Add(labelUsers);
+            panelTop.Dock = DockStyle.Top;
+            panelTop.Location = new Point(0, 0);
+            panelTop.Margin = new Padding(5);
+            panelTop.Name = "panelTop";
+            panelTop.Padding = new Padding(10);
+            panelTop.Size = new Size(684, 70);
+            panelTop.TabIndex = 1;
+            // 
+            // labelUsers
+            // 
+            labelUsers.Dock = DockStyle.Fill;
+            labelUsers.Location = new Point(10, 10);
+            labelUsers.Name = "labelUsers";
+            labelUsers.Size = new Size(664, 50);
+            labelUsers.TabIndex = 0;
+            labelUsers.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(684, 561);
+            Controls.Add(panel1);
+            Controls.Add(panelTop);
+            Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            Margin = new Padding(5);
+            Name = "FormMain";
+            Text = "Пользователи";
+            Load += FormMain_Load;
+            panel1.ResumeLayout(false);
+            panelTop.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panelTop;
+        private Label labelUsers;
+        private Label LabelInfAboutUsers;
+    }
+}
